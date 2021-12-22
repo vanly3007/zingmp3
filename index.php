2d3aceff7b08a02abd1b1d2dd763a6ce23c5880c
vanly3007 <vanly3002@gmail.com>
2021-12-22 21:54:52 +0700

    abc

---
+++ index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" >
+  <link href="https://fonts.googleapis.com/css2?family=Material+Icons+Outlined" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+   
+  <title>Document</title>
+</head>
+<body>
+    <!-- Phần Header-->
+    <div class="header">
+      <div class="header_left">
+        <span class="material-icons-outlined">
+          west
+          </span>
+        <span class="material-icons-outlined">
+          east
+          </span>
+      </div>
+      <div class="header_search">
+        <span class="material-icons-outlined">
+          search
+          </span>
+        <input type="text" class="header_search-input" placeholder="Nhập tên bài hát, nghệ sĩ hoặc MV...">
+
+      </div>
+      <div class="header_right">
+        <div class="header__topic">
+          <i class=" header__topic-hover fas fa-tshirt" style="color:hotpink"></i>
+          <div class="header__topic-notify">
+            <p>Chủ đề</p>
+          </div> 
+          
+        </div>
+        
+        
+        <div class="header__upload" , title="Tải lên">
+          <span class="material-icons-outlined">
+            file_upload
+            </span>
+            <div class="header__upload-notify">
+              <p>Tải lên</p>
+            </div>
+        </div>
+        <div class="header__setting" , title="Cài đặt">
+          <span class="material-icons-outlined">
+            settings
+            </span>
+            <div class="header__setting-notify">
+              <p>Cài đặt</p>
+            </div>
+        </div>
+        <div class="header__avatar">
+          <i class=" fas fa-user-circle fa-4x"style="color:#dadada;" ></i>
+        </div>
+        
+      </div>
+    </div>
+    <!-- phần main-->
+    <div class="main_container">
+      <div class="main_text">
+        <h5>LỰA CHỌN HÔM NAY</h5>
+      </div>
+      <div class="main_articles">
+       <div class="main_article">
+          <div  class="main_article-img">
+            <a href="index.html">
+                 <img src="css/images/usuk-top.jpg" style="width:200px" alt="">
+
+            </a>
+            <div class="main_article-icon">
+              <span class="material-icons-outlined">
+                favorite_border
+                </span>
+                <span class=" material-icons-outlined">
+                  play_circle_outline
+                  </span>
+                  <span class="material-icons-outlined">
+                    more_horiz
+                    </span>
+            </div>
+        </div>
+          <a  class="main_article-title "href="">Top hits of 2021</a>
+           <p class="main_article-topic">Những ca khúc gây sốt tại top hits of 2021 cùng hàng loạt ca sĩ...</p>
+        </div>
+        <div class="main_article">
+          <div  class="main_article-img">
+            <a href="index.html">
+                 <img src="css/images/blackpink.jpg" style="width:200px" alt="">
+            </a>
+            <div class="main_article-icon">
+              <span class="material-icons-outlined">
+                favorite_border
+                </span>
+                <span class=" material-icons-outlined">
+                  play_circle_outline
+                  </span>
+                  <span class="material-icons-outlined">
+                    more_horiz
+                    </span>
+            </div>
+        </div>
+          
+          <a  class="main_article-title "href="">Sôi động cùng BlackPink</a>
+           <p class="main_article-topic">Cùng thưởng thức album mới nhất của các cô nàng BlackPink...</p>
+        </div>
+        <div class="main_article">
+          <div  class="main_article-img">
+            <a href="index.html">
+                 <img src="css/images/usuk-top.jpg" style="width:200px" alt="">
+            </a>
+            <div class="main_article-icon">
+              <span class="material-icons-outlined">
+                favorite_border
+                </span>
+                <span class=" material-icons-outlined">
+                  play_circle_outline
+                  </span>
+                  <span class="material-icons-outlined">
+                    more_horiz
+                    </span>
+            </div>
+        </div>
+          
+          <a  class="main_article-title "href="">Top hits of 2021</a>
+           <p class="main_article-topic">Những ca khúc gây sốt tại top hits of 2021 cùng hàng loạt ca sĩ...</p>
+        </div>
+        <div class="main_article">
+          <div  class="main_article-img">
+            <a href="index.html">
+                 <img src="css/images/usuk-top.jpg" style="width:200px" alt="">
+            </a>
+            <div class="main_article-icon">
+              <span class="material-icons-outlined">
+                favorite_border
+                </span>
+                <span class=" material-icons-outlined">
+                  play_circle_outline
+                  </span>
+                  <span class="material-icons-outlined">
+                    more_horiz
+                    </span>
+            </div>
+        </div>
+          
+          <a  class="main_article-title "href="">Top hits of 2021</a>
+           <p class="main_article-topic">Những ca khúc gây sốt tại top hits of 2021 cùng hàng loạt ca sĩ...</p>
+        </div>
+        <div class="main_article">
+          <div  class="main_article-img">
+            <a href="index.html">
+                 <img src="css/images/usuk-top.jpg" style="width:200px" alt="">
+            </a>
+            <div class="main_article-icon">
+              <span class="material-icons-outlined">
+                favorite_border
+                </span>
+                <span class=" material-icons-outlined">
+                  play_circle_outline
+                  </span>
+                  <span class="material-icons-outlined">
+                    more_horiz
+                    </span>
+            </div>
+        </div>
+          
+          <a  class="main_article-title "href="">Top hits of 2021</a>
+           <p class="main_article-topic">Những ca khúc gây sốt tại top hits of 2021 cùng hàng loạt ca sĩ...</p>
+        </div>
+
+
+
+      </div>
+    </div>
+
+</body>
+</html>
